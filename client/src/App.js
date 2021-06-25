1745e91c4e63ed1650a3b3275ed8188e975d490d
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-24 20:45:59 -0500

    added search and saved pages to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -12,10 +13,13 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path={["/", "/books"]}>
-            <Books />
+            <Search />
           </Route>
           <Route exact path="/books/:id">
             <Detail />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
           </Route>
           <Route>
             <NoMatch />
