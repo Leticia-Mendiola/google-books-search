398c0324a98da433b0cd9e348b9a1a3c1d7bddd7
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-24 20:09:58 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(
 );
 
 // Define API routes here
-
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
