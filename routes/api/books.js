b80d43bf274e142e80ddecb6cfb2d3d11fd6d3dd
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-26 23:00:49 -0500

    added searchByTitle to routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 // Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+router.route("/").get(booksController.searchByTitle);
+
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
