a1bb4d62f83f141dac4f91e39e93af92da6fac5f
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-26 22:49:16 -0500

    changed bookSchema to more acurately match googleBooks API return data

--- models/book.js
+++ models/book.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  discription: String,
-  image: String,
-  link: {type: String, required: true},
+  googleBooksId: { type: String },
+    googleBooksLink: { type: String },
+    volumeInfo: {
+        title: { type: String },
+        authors: [{ type: String }],
+        publisher: { type: String },
+        publishedDate: { type: String },
+        description: { type: String },
+        categories: [{ type: String }],
+        imageLinks: {
+            smallThumbnail: { type: String },
+            thumbnail: { type: String }
+        },
+        previewLink: { type: String }
+    },
 });
 
 const Book = mongoose.model("Book", bookSchema);
